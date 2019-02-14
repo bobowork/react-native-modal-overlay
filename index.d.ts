331cac02cbf5329c6abe82cc5ec0aa282950080a
bobowork <bwang@metroland.com>
2019-02-14 02:43:38 -0500

    update

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,4 @@
 declare module 'react-native-modal-overlay' {
-    declare class OverlayComponent extends React.Component<any> {}
-    declare const OverlayBase: Constructor<NativeMethodsMixin> & typeof OverlayComponent;
-    export class Overlay extends OverlayBase {}
+    import Overlay from ".";
+    export default Overlay
 }
